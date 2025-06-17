3c88d6a15a955fa449b39c9c96a7532211d82f80
Luis Gallegos <luisfergallegos@gmail.com>
2025-06-16 18:57:48 -0600

    PRD

--- src/styles/Constants.jsx
+++ src/styles/Constants.jsx
@@ -1,2 +1,7 @@
 export const urlApi = 'http://149.50.147.12:8080/api/';
 export const urlHost = 'http://149.50.147.12:8080/';
+
+/* // PRD
+export const urlApi = 'https://api.plannersday.com/api/';
+export const urlHost = 'https://api.plannersday.com/'; 
+*/
