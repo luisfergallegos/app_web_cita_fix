d648a17ebe93d21eedc8511fc3c63bb7d24dac1d
Luis Gallegos <luisfergallegos@gmail.com>
2025-06-18 00:28:17 -0600

    Update
    PRD Constants

--- src/styles/Constants.jsx
+++ src/styles/Constants.jsx
@@ -1,7 +1,7 @@
+/* // DEV
 export const urlApi = 'http://149.50.147.12:8080/api/';
 export const urlHost = 'http://149.50.147.12:8080/';
-
-/* // PRD
+ */
+// PRD
 export const urlApi = 'https://api.plannersday.com/api/';
 export const urlHost = 'https://api.plannersday.com/'; 
-*/
